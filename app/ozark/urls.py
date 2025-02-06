f4c3a13f0dcbbd26a7200f941013905d211e9810
David Barajas <david.barajas@yotepresto.com>
2025-02-06 12:13:10 -0600

    Fix urls

--- app/ozark/urls.py
+++ app/ozark/urls.py
@@ -19,10 +19,10 @@ urlpatterns = [
     path('blacklisted_persons/<int:pk>/', BlacklistPersonViewSet.as_view({'get': 'retrieve', 'delete': 'destroy'})),
 
     # Profile Management
-    path('profiles/', ProfileViewSet.create_profile, name='create_profile'),
-    path('profiles/<int:id>/', ProfileViewSet.get_profile_by_id, name='get_profile_by_id'),
-    path('profiles/<int:id>/', ProfileViewSet.update_profile_base_info, name='update_profile_base_info'),
-    path('profiles/<int:id>/attribute', ProfileViewSet.create_attribute_in_profile, name='create_attribute_in_profile'),
-    path('profiles/<int:id>/attribute/<int:attribute_id>/', ProfileViewSet.delete_attribute_from_profile, name='delete_attribute_from_profile'),
-    path('profiles/<int:id>/', ProfileViewSet.delete_profile, name='delete_profile'),
+    #path('profiles/', ProfileViewSet.create_profile, name='create_profile'),
+    #path('profiles/<int:id>/', ProfileViewSet.get_profile_by_id, name='get_profile_by_id'),
+    #path('profiles/<int:id>/', ProfileViewSet.update_profile_base_info, name='update_profile_base_info'),
+    #path('profiles/<int:id>/attribute', ProfileViewSet.create_attribute_in_profile, name='create_attribute_in_profile'),
+    #path('profiles/<int:id>/attribute/<int:attribute_id>/', ProfileViewSet.delete_attribute_from_profile, name='delete_attribute_from_profile'),
+    #path('profiles/<int:id>/', ProfileViewSet.delete_profile, name='delete_profile'),
 ]
